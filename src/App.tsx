0d6958d5c9a8d053594a1ceba42d0bb478aa3bd8
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-06-27 12:01:12 -0400

    hello

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      hello world
+      hello wold
+    </div>
+  )
+}
+
+export default App
+
+
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/PaneruVipin/ShowFinder.git
+git push -u origin main
