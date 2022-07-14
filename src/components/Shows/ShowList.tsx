e5d054335d7a06d2edb13b2e7cc13d13beb96034
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-07-14 13:51:08 -0700

    clone

--- src/components/Shows/ShowList.tsx
+++ src/components/Shows/ShowList.tsx
@@ -11,6 +11,7 @@ const ShowList: FC<ShowListProps> = ({shows,query}) => {
   return (
     <div className="flex flex-wrap gap-x-1 gap-y-6 md:gap-y-12 md:justify-between justify-center">
       { shows.map((s)=><Show key={s.id} query={query} show={s}></Show>)}
+      <span className="grow"></span>
     </div>
   )
 };
